729ffed88bc3d82b0706dd79fa49b3b4f2fd408e
Qiao He <qiaoh3@uci.edu>
2017-08-03 15:19:29 -0700

    Update my_fork.c

--- my_fork.c
+++ my_fork.c
@@ -1,3 +1,16 @@
+/*
+Unix process creation: Write a program, called my_fork, that uses fork() to create a total of 4 processes - 
+each printing one letter of the alphabet from A to D K times.  K will be a command argument, but should default to 10000. 
+(for debugging only: have each process print it's process id (PID) when it is created see getpid().) 
+Also have each process flush the output after printing each character with fflush(stdout). Read about fork() in man or google. 
+Be sure you do not leave any processes running.  You can check this with the command, ps -ef or ps aux. 
+Note a parent process must wait() for each child after it has terminated, or that child will become a zombie.
+Here is an example call (note the letters should be a jumbled mess and should be different order every time you call your program). 
+For debugging, you can put a call to sleep(1) after you flush each character which should force another process to run
+*/
+
+
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
